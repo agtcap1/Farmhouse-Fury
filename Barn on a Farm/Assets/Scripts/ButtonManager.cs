afe4efea5407c5caef2d077973b37f6b138a481a
agtcap1 <agtcap1@yahoo.com>
2017-04-20 21:51:49 -0400

    Started
    Strated making the main menu and "press any key to start" screen, will
    be learning how to delay input acceptance so they have to wait for an
    animation to finish in order to press anything.

---
+++ Barn on a Farm/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+    public Transform Canvas;
+    public Transform Menu;
+
+    public void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Canvas.gameObject.SetActive(false);
+            Menu.gameObject.SetActive(true);
+
+        }
+            
+    }
+
+    public void NewGameBtn(string NewGameLevel)
+    {
+        SceneManager.LoadScene(NewGameLevel);
+    
+    }
+
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+
+
+}
