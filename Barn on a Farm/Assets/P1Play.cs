4939b8a95150df05fca645fc1dfacfb86cc33445
jpcolfer <jpcolfer99@gmail.com>
2017-05-11 01:36:49 -0400

    Added code to Player Characters
    Added some Code + Beginnings of actual game

---
+++ Barn on a Farm/Assets/P1Play.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingGame : MonoBehaviour
+{
+
+    int Switch = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        //Player 1
+        if (Input.GetButtonDown("Q") && Switch == 0)
+        {
+            var pos = transform.position;
+            pos.x += 45;
+            transform.position = pos;
+            pos.y += 20;
+            transform.position = pos;
+            Switch = 1;
+        }
+
+        else if (Input.GetButtonDown("Q") && Switch == 1)
+        {
+            var pos = transform.position;
+            pos.x -= 45;
+            transform.position = pos;
+            pos.y -= 20;
+            transform.position = pos;
+            Switch = 0;
+        }
+
+    }
+}
---
+++ Barn on a Farm/Assets/Player2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+
+    int Switch = 0;
+    int Winner = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        //Player 1
+        if (Input.GetButtonDown("Z") && Switch == 0)
+        {
+            var pos = transform.position;
+            pos.x += 45;
+            transform.position = pos;
+            pos.y += 35;
+            transform.position = pos;
+            Switch = 1;
+            Winner++;
+        }
+
+        else if (Input.GetButtonDown("Z") && Switch == 1)
+        {
+            var pos = transform.position;
+            pos.x -= 45;
+            transform.position = pos;
+            pos.y -= 35;
+            transform.position = pos;
+            Switch = 0;
+            Winner++;
+        }
+
+        if (Winner == 50)
+        {
+            Time.timeScale = 0;
+        }
+
+    }
+}
